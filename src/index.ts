da5507928863dbc6c9616ade518fee0cee41cde4
aleksandre <faleqs33@gmail.com>
2025-07-25 20:02:03 +0400

    new changes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-var x = 5;
-console.log(x);
+function logData(data: unknown) {
+  console.log("Data:", data);
+}
+
+logData({ key: "value" });
+
+
+
+async function fetchData(): Promise<string> {
+  return Promise.resolve("Hello TypeScript");
+}
+
+fetchData().then((message) => console.log(message));
+
+
+class Example {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
+
+const example = new Example("Test");
+console.log(example.getName());
