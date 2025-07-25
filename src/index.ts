a07a0a8ac878cc0bd7513f9668075a6da3f6ab90
aleksandre <faleqs33@gmail.com>
2025-07-25 20:07:33 +0400

    new changes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-function logData(data: unknown) {
+function logData(data: any) {
   console.log("Data:", data);
 }
 
 logData({ key: "value" });
 
+let count = 0;
+count += 1;
 
+const unusedVarTS = "I am not used";
 
 async function fetchData(): Promise<string> {
   return Promise.resolve("Hello TypeScript");
@@ -12,6 +15,7 @@ async function fetchData(): Promise<string> {
 
 fetchData().then((message) => console.log(message));
 
+const greeting: string = "Hello, TypeScript Lint!";
 
 class Example {
   private name: string;
